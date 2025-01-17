28af5d864c12f2cd3c99cefcad9c045d42cfc4a7
PiSera1 <kevinwellens24@gmail.com>
2025-01-17 10:49:59 +0100

    1. / Felder / Wertzuweisung / toString()

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,6 +2,17 @@ package com.cc.java;
 
 public class Cat {
 
+    public String firstName;
+    public String furColor;
+    public int age; 
+
+
+
+
+
+
+
+
 
     public Cat getInstanceVariable(){
        return this;
